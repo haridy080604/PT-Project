217488f67874fc6237d067e4d7fa417a4e30c491
haridy080604 <Ahmed.Mansour04@eng-st.cu.edu.eg>
2024-11-14 23:16:50 +0200

    Edits (#3)
    * I added the rest of the icons
    
    * I added action to icons and make them print a message in the status bar
    
    * We finished getCellClicked and add some test cases and edit the way of drawing of output

--- Input.cpp
+++ Input.cpp
@@ -183,9 +183,8 @@ CellPosition Input::GetCellClicked() const
 		{
 			///TODO: SetHCell and SetVCell of the object cellPost appropriately
 			//       using the coordinates x, y and the appropriate variables of the UI_Info Object (UI)
-			
-
-
+			cellPos.SetHCell(x / UI.CellWidth);
+			cellPos.SetVCell((y - UI.ToolBarHeight) / UI.CellHeight);
 		}
 	}
 

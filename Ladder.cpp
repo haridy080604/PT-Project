0d97efb077d20f4b080333c26f9e55bd8ff32084
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-14 14:49:19 +0200

    Update Ladder.cpp

--- Ladder.cpp
+++ Ladder.cpp
@@ -1,37 +1,65 @@
 #include "Ladder.h"
+#include "Output.h"
+#include "Player.h"
+#include "Input.h"
 
-Ladder::Ladder(const CellPosition & startCellPos, const CellPosition & endCellPos) : GameObject(startCellPos)
+Ladder::Ladder(const CellPosition& startCellPos, const CellPosition& endCellPos) : GameObject(startCellPos)
 {
-	this->endCellPos = endCellPos;
 
-	///TODO: Do the needed validation
+	//check validation
+	if (endCellPos.VCell() < this->position.VCell() && endCellPos.HCell() == this->position.HCell())
+		this->endCellPos = endCellPos;
+
 }
+Ladder::Ladder() {
+	this->endCellPos = CellPosition::GetCellPositionFromNum(1); //Default Constructor used in Open Grid
+
+}
+
 
 void Ladder::Draw(Output* pOut) const
 {
 	pOut->DrawLadder(position, endCellPos);
 }
 
-void Ladder::Apply(Grid* pGrid, Player* pPlayer) 
+void Ladder::Apply(Grid* pGrid, Player* pPlayer)
 {
-	
-
-	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 
 
-	// == Here are some guideline steps (numbered below) to implement this function ==
-	
-	// 1- Print a message "You have reached a ladder. Click to continue ..." and wait mouse click
+	///DONE: Implement this function as mentioned in the guideline steps (numbered below) below
+
+	// 1- Print a Proper Message
+	pGrid->PrintErrorMessage("You have reached a ladder. Click to continue ...");
 
 	// 2- Apply the ladder's effect by moving the player to the endCellPos
-	//    Review the "pGrid" functions and decide which function can be used for that
-	
+	pGrid->UpdatePlayerCell(pPlayer, GetEndPosition());
 }
 
 CellPosition Ladder::GetEndPosition() const
 {
 	return endCellPos;
 }
+
+void Ladder::Save(ofstream& OutFile, GameObjectsType Obj) const {
+	if (Obj == OBJ_LADDER) // check if Obj is Ladder
+	{
+		GameObject::Save(OutFile, Obj); //print the start Position of Ladder 
+		OutFile << GetEndPosition().GetCellNum() << '\n'; // print the The end Positon of ladder
+	}
+}
+
+GameObject* Ladder::Load(ifstream& InFile) {
+
+	GameObject::Load(InFile);
+
+	int  endPosCellNum;
+
+	InFile >> endPosCellNum; //read
+
+	endCellPos = CellPosition::GetCellPositionFromNum(endPosCellNum);
+	return this;
+}
+
 
 Ladder::~Ladder()
 {

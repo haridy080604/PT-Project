c7cfa818d7f9b7a2bd328cd354893a788713a88a
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-17 21:07:00 +0200

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,41 +1,59 @@
 #include "Card.h"
+#include"Input.h"
+#include"CellPosition.h"
+#include "CardOne.h"
+//#include "CardTwo.h"
+//#include "CardThree.h"
+//#include "CardFour.h"
+//#include "CardFive.h"
+//#include "CardSix.h"
+//#include "CardSeven.h"
+//#include "CardEight.h"
+#include "CardNine.h"
+#include "CardTen.h"
+#include "CardEleven.h"
+#include "CardTwelve.h"
+#include "CardThirteen.h"
 
-
-Card::Card(const CellPosition & pos) : GameObject(pos) // sets the cell position of the GameObject
+Card::Card(const CellPosition& pos) : GameObject(pos) // sets the cell position of the GameObject
 {
+}
+
+Card::Card() {
+	//Default Constructor Use In OpenGridAction
 }
 
 void Card::SetCardNumber(int cnum)
 {
-	cardNumber = cnum; // needs validation
+	// check the validation 
+	if (cardNumber > 0 && cardNumber <= 13)
+		cardNumber = cnum;
 }
 
-int Card::GetCardNumber() const
-{
-	return cardNumber;
-}
+int Card::GetCardNumber() const { return cardNumber; }
 
 void Card::Draw(Output* pOut) const
 {
-
-	///TODO: call the appropriate Ouput function that draws a cell containing the "cardNumber" in "position"
-
-
+	///DONE: call the appropriate Ouput function that draws a cell containing the "cardNumber" in "position"
+	pOut->DrawCell(position, cardNumber);
 }
 
-void Card::ReadCardParameters(Grid * pGrid)
+void Card::ReadCardParameters(Grid* pGrid)
 {
+
 	// we should not make it pure virtual because some Cards doesn't have parameters
 	// and if we make it pure virtual, that will make those Cards abstract classes
 }
 
-void Card::Apply(Grid* pGrid, Player* pPlayer) 
+void Card::Apply(Grid* pGrid, Player* pPlayer)
 {
-	// As written below the "Roll Dice" action in the document ==> Check the Project Document
-	// "If a player reaches a card of any other type", the following message should be printed then wait mouse click
-
-	pGrid->PrintErrorMessage("You have reached card " + to_string(cardNumber) + ". Click to continue ...");
+	//print proper Message 
+	//this will be overridden by all cards
+	pGrid->PrintErrorMessage("You have reached card " + to_string(cardNumber) + ", Click to continue ...");
 }
+
+
+
 
 Card::~Card()
 {

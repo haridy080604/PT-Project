15bdb8c63c6395e0d06d5cf48929a70ae9d6dd3b
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-17 21:07:51 +0200

    Update GameObject.cpp

--- GameObject.cpp
+++ GameObject.cpp
@@ -10,28 +10,14 @@ GameObject::GameObject()
 	position = CellPosition::GetCellPositionFromNum(1); // Sets Position
 }
 
-bool GameObject::IsOverlapping(const Grid* pGrid, GameObject* newObj) const {
-	return true;//as default value which have no value actually in program
+bool GameObject::IsOverlapping(GameObject* newObj) const
+{
+	return false;
 }
 
 CellPosition GameObject::GetPosition() const
 {
 	return position;
-}
-
-//print the start position of any object as it used in all objects
-void GameObject::Save(ofstream& OutFile, GameObjectsType Obj) const {
-	OutFile << GetPosition().GetCellNum() << ' ';
-}
-
-
-GameObject* GameObject::Load(ifstream& InFile) {
-	int startPosCellNum;
-
-	InFile >> startPosCellNum; //read
-
-	position = CellPosition::GetCellNumFromPosition(startPosCellNum);
-	return NULL;
 }
 
 

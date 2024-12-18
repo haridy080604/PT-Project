2ed62c9aa357a8a8ad220a0fa4499ed5216f2df4
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-19 00:34:59 +0200

    Update CardThree.cpp

--- CardThree.cpp
+++ CardThree.cpp
@@ -3,29 +3,29 @@
 CardThree::CardThree(const CellPosition& pos) : Card(pos)
 {
 
-	int card_number = 3;
+	cardNumber = 3;
 
 }
 
 void CardThree::Apply(Grid* pGrid, Player* pPlayer)
-{   
+{
 	Card::Apply(pGrid, pPlayer);
 	int dice_number = 0;
 	int current_pos = pPlayer->GetCell()->GetCellPosition().GetCellNum();
 	GameObject* pObject = (GameObject*)pGrid->GetNextLadder(pPlayer->GetCell()->GetCellPosition());
 
-   
+
 	if (pObject != NULL)
 	{
 		pGrid->UpdatePlayerCell(pPlayer, pObject->GetPosition());
-		
 
-	
+
+
 	}
 
-	
 
- }
+
+}
 
 CardThree::~CardThree()
 {

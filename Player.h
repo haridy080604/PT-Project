28269d809611f41ff53b1abe48ea331424c89be2
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-19 00:25:00 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,25 +5,25 @@
 
 class Player
 {
-	Cell * pCell;		   // pointer to the current Cell of the player
+	Cell* pCell;		   // pointer to the current Cell of the player
 
 	const int playerNum;   // the player number (from 0 to MaxPlayerCount-1)
-	                       // player number does NOT change after construction (const.)
+	// player number does NOT change after construction (const.)
 
 	int stepCount;		   // step count which is the same as his cellNum: from 1 to NumVerticalCells*NumHorizontalCells
 	int wallet;		       // player's wallet (how many coins he has -- integer)
 	int justRolledDiceNum; // the current dice number which is just rolled by the player
 	int turnCount;         // a counter that starts with 0, is incremented with each dice roll
-	                       // and reset again when reached 3
-	                       // it is used to indicate when to move and when to add to your wallet
-	
+	// and reset again when reached 3
+	// it is used to indicate when to move and when to add to your wallet
+
 public:
 
-	Player(Cell * pCell, int playerNum); // Constructor making any needed initializations
+	Player(Cell* pCell, int playerNum); // Constructor making any needed initializations
 
 	// ====== Setters and Getters ======
 
-	void SetCell(Cell * cell);		// A setter for the pCell
+	void SetCell(Cell* cell);		// A setter for the pCell
 	Cell* GetCell() const;			// A getter for the pCell
 
 	void SetWallet(int wallet);		// A setter for the wallet
@@ -32,9 +32,9 @@ public:
 	int GetTurnCount() const;		// A getter for the turnCount
 
 	int GetJustRolledDiceNum();
-	
+
 	void SetTurnCount(int new_turn_count);
-	
+
 
 	///TODO: You can add setters and getters for data members here (if needed)
 
@@ -46,12 +46,11 @@ public:
 
 	// ====== Game Functions ======
 
-	void Move(Grid * pGrid, int diceNumber);	// Moves the Player with the passed diceNumber 
-	                                            // and Applies the Game Object's effect (if any) of the end reached cell 
-	                                            // for example, if the end cell contains a ladder, take it
-	
-	void AppendPlayerInfo(string & playersInfo) const; // Appends player's info to the input string, 
-	                                                   // for example: P0(wallet, turnCount)
+	void Move(Grid* pGrid, int diceNumber);	// Moves the Player with the passed diceNumber 
+	// and Applies the Game Object's effect (if any) of the end reached cell 
+	// for example, if the end cell contains a ladder, take it
+
+	void AppendPlayerInfo(string& playersInfo) const; // Appends player's info to the input string, 
+	// for example: P0(wallet, turnCount)
 
 };
-

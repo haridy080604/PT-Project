3168a4bf4a70e19a07eb02daa74f1bb6a71eb87c
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-19 00:36:05 +0200

    Update CardTwo.cpp

--- CardTwo.cpp
+++ CardTwo.cpp
@@ -2,7 +2,7 @@
 
 CardTwo::CardTwo(const CellPosition& pos) : Card(pos) // set the cell position of the card
 {
-	cardNumber = 1; // set the inherited cardNumber data member with the card number (1 here)
+	cardNumber = 2; // set the inherited cardNumber data member with the card number (1 here)
 }
 
 CardTwo::~CardTwo(void)
@@ -13,16 +13,14 @@ void CardTwo::ReadCardParameters(Grid* pGrid)
 {
 
 
-	Input* pIn;
-	Output* pOut;
-
-	pOut->PrintMessage("New CardTwo: Enter its wallet amount\n");
 
 
-	walletAmount = pIn->GetInteger(pOut);
-	pOut->ClearStatusBar();
+	pGrid->GetOutput()->PrintMessage("New CardTwo: Enter its wallet amount");
+	walletAmount = pGrid->GetInput()->GetInteger(pGrid->GetOutput());
 
-	
+
+	pGrid->GetOutput()->ClearStatusBar();
+
 
 
 	// == Here are some guideline steps (numbered below) (numbered below) to implement this function ==
@@ -50,11 +48,10 @@ void CardTwo::Apply(Grid* pGrid, Player* pPlayer)
 
 
 	// == Here are some guideline steps (numbered below) (numbered below) to implement this function ==
-	Card:: Apply(pGrid, pPlayer);
+	Card::Apply(pGrid, pPlayer);
 	pPlayer->SetWallet(pPlayer->GetWallet() + walletAmount);
 	// 1- Call Apply() of the base class Card to print the message that you reached this card number
 
 	// 2- Decrement the wallet of pPlayer by the walletAmount data member of CardTwo
 
 }
-

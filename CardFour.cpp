37faf83d52b69b20b9a987953edb86e311f20d35
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-19 00:29:52 +0200

    Update CardFour.cpp

--- CardFour.cpp
+++ CardFour.cpp
@@ -3,14 +3,14 @@
 CardFour::CardFour(const CellPosition& pos) : Card(pos)
 {
 
-	int card_number = 4;
+	cardNumber = 4;
 
 }
 
 void CardFour::Apply(Grid* pGrid, Player* pPlayer)
 {
 	Card::Apply(pGrid, pPlayer);
-	
+
 
 
 	/*if (pGrid->GetNextSnake(pPlayer->GetCell()->GetCellPosition()) != NULL)

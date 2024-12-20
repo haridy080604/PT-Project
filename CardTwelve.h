f7397fd092d3408edfc50b2c7da39fb6847305a2
M-Amir133 <Mohamed.Fahmy051@eng-st.cu.edu.eg>
2024-12-20 19:53:44 +0200

    Update CardTwelve.h

--- CardTwelve.h
+++ CardTwelve.h
@@ -11,8 +11,8 @@ private:
     static Player* pOwner;     // Owner of all Card Twelve stations
     static int CardPrice;      // Price of the station
     static int Fees;           // Fees to be paid when other players land
-    static bool PrintOnce;     // Ensure price is printed only once
-    static bool ReadOnce;      // Ensure price is read only once
+    // static bool PrintOnce;     // Ensure price is printed only once
+    // static bool ReadOnce;      // Ensure price is read only once
 
 public:
     CardTwelve(const CellPosition& pos);  // Parameterized constructor
@@ -23,9 +23,9 @@ public:
     virtual void Apply(Grid* pGrid, Player* pPlayer);
   
 
-    // Additional methods
-    static void EndPrint();
-    static void EndRead();
+    // // Additional methods
+    // static void EndPrint();
+    // static void EndRead();
 };
 
 #endif // CARD_Twelve_H
